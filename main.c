0fbef35b044824a6c499ee8e0721e0f92bc9642e
PICmajster <picmajster.blog@gmail.com>
2019-02-09 19:03:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -22,13 +22,13 @@ void main(void) {
  /* Initialize the SAM system */
 SystemInit();
 /*Set pin PB11 direction to Output*/
-PORT->Group[1].DIRSET.reg |= PORT_PB11; 
+PORT->Group[1].DIRSET.reg = PORT_PB11; 
 
    
    while (1)
    {
    /*Toggle pin PB11*/
-   PORT->Group[1].OUTTGL.reg |= PORT_PB11; 
+   PORT->Group[1].OUTTGL.reg = PORT_PB11; 
    _delay_ms(1000);
    }
 
